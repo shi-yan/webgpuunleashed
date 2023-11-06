3ca81f6c74c2551b426e90fa493d94acecc8179c
Shi Yan <billconan@gmail.com>
2023-11-05 19:37:02 -0800

    update

--- test_base/test.glsl
+++
@@ -1,3 +0,0 @@
-vec3 main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-}
--- test_base/test.js
+++
@@ -1,5 +0,0 @@
-console.log('debug print');
-
-const shader = document.getElementById('shader');
-
-console.log('print shader', shader.innerText);
