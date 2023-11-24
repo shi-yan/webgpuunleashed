b09941989a48dbe66959f089faabf450a9759039
Shi Yan <billconan@gmail.com>
2023-11-23 20:27:19 -0800

    fix seams

--- 5_4_mega_texture/preprocess.py
+++ 5_4_mega_texture/preprocess.py
@@ -2,7 +2,8 @@ import PIL.Image
 import math
 image = PIL.Image.open('crab_nebula.png')
 image = image.crop((0, 0, math.ceil(image.width/256)*256,math.ceil(image.height/ 256)*256))
-for lv in range(0,math.floor(math.log(256,2)+1)):
+padding = 2
+for lv in range(0,math.floor(math.log(256,2))+1):
     
     resize_ratio = math.pow(2, lv)
     scaled_width = int(image.width / resize_ratio)
@@ -13,6 +14,6 @@ for lv in range(0,math.floor(math.log(256,2)+1)):
     v = math.ceil(scaled_height / 256)
     for y in range(0,v):
         for x in range(0,h):
-            cropped_image = resized_image.crop((x*256, y*256, (x+1)*256,(y+1)* 256))
+            cropped_image = resized_image.crop((x*256-padding, y*256-padding, (x+1)*256+padding,(y+1)* 256+padding))
             cropped_image.save('../crab_nebula/crab_'+str(lv)+'_'+str(y)+'_'+str(x)+'.png')
 
